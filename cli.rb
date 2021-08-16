1d41eda0c9c66e2762e5d14c0c69936fd8a8c12c
Anchieta Júnior <santosjr87@gmail.com>
2021-08-16 10:35:19 -0300

    Refactoring cli

--- cli.rb
+++ cli.rb
@@ -3,15 +3,16 @@
 require 'readline'
 require_relative './lib/account'
 
+@times_to_show_message = 1
 @account = Account.new
 
 MENU = [
-  '> menu or 0 -> Show options',
-  '> payable or 1 -> Add a payable account',
-  '> receivable or 2 -> Add a receivable account',
-  '> balance or 3 -> Show the current balance of the month',
-  '> report or 4 -> Show a month budget report',
-  '> exit or 5 -> Quit the CLI'
+  '> (1 or menu) -> Show options',
+  '> (2) -> Add a payable account',
+  '> (3) -> Add a receivable account',
+  '> (4) -> Show a report by month and year',
+  '> (5) -> Show a report by year',
+  '> (0) -> Quit the CLI'
 ].freeze
 
 CATEGORIES = Account::CATEGORIES.map { |key, value| "#{value} => #{key}" }.freeze
@@ -20,44 +21,52 @@ def add_payable
   params = {}
   params[:description] = Readline.readline('Payable description > ')
   params[:amount] = Readline.readline('Amount > ')
-  params[:deadline_year] = Readline.readline('Deadline month (2021..) > ')
-  params[:deadline_month] = Readline.readline('Deadline month (1-12) > ')
-  params[:deadline_day] = Readline.readline('Deadline day (1-31) > ')
-  puts 'Choose one one these categories'
-  puts CATEGORIES
-  params[:category] = Readline.readline(
-    '> '
-  )
-  params[:status] = Readline.readline('Status (Opened/Finished) > ')
+  params[:deadline] = Readline.readline('Year-Month-Date like (2021-1-1) > ')
+  @account.categories_table
+  params[:category] = Readline.readline('Category > ')
   @account.add_payable(params)
-  puts 'Added a payable account.'
+  puts 'Ok!'
+  @account.reload_file
+end
+
+def add_receivable
+  params = {}
+  params[:description] = Readline.readline('Receivable description > ')
+  params[:amount] = Readline.readline('Amount > ')
+  params[:deadline] = Readline.readline('Year-Month-Date like (2021-1-1) > ')
+  params[:category] = 'receivable'
+  @account.add_receivable(params)
+  puts 'Ok!'
+  @account.reload_file
 end
 
 loop do
-  puts 'Type "menu" to see all the options'
+  unless @times_to_show_message > 1
+    puts 'Type menu or 1 to see the options'
+    @times_to_show_message = 2
+  end
   line = Readline.readline('> ')
   break if line.nil? || line == 'exit'
 
   case line
-  when 'menu', '0'
+  when 'menu', '1'
     puts MENU
-  when 'payable', '1'
+  when '2'
     add_payable
-  when 'receivable', '2'
-    account.add_receivable
-  when 'balance', '3'
-    account.balance
-  when 'report', '4'
-    month = Readline.readline('Type the month > ')
-    year = Readline.readline('Type the year > ')
-    puts "Relatório do mês #{month}, de #{year}"
-    @account.month_budget(month.to_i, year.to_i)
+  when '3'
+    add_receivable
+  when '4'
+    date = Readline.readline('Year-Month like (2021-1) > ')
+    puts "Searching accounts.. \n\n"
+    @account.month_budget(date)
   when '5'
+    year = Readline.readline('Year like (2021) > ')
+    puts "Searching accounts.. \n\n"
+    @account.year_budget(year.to_i)
+  when '0'
     puts 'Goodbye, see you soon'
     break
   else
-    puts 'Opção não encontrada'
+    puts 'Type 1 to see menu with options'
   end
 end
-
-private
